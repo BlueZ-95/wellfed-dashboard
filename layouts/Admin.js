f2fd341bfbc8899afeca8cd761546556b3a8f5c7
Saad Patel <spatel@horizontal.com>
2022-10-05 22:39:16 +0530

    init

--- layouts/Admin.js
+++ layouts/Admin.js
@@ -10,9 +10,9 @@ import FooterAdmin from "components/Footers/FooterAdmin.js";
 export default function Admin({ children }) {
   return (
     <>
-      <Sidebar />
-      <div className="relative md:ml-64 bg-blueGray-100">
-        <AdminNavbar />
+      {/* <Sidebar /> */}
+      <div className="relative bg-blueGray-100">
+        {/* <AdminNavbar /> */}
         {/* Header */}
         <HeaderStats />
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
---
+++ mocks/consumer.mock.js
@@ -0,0 +1,12 @@
+export default {
+  columns: [
+    "Course ID",
+    "Purchase Status",
+    "Course Name",
+    "Total Screen Time",
+    "Course Status",
+    "QR Certificate",
+    "View Certificate",
+    "Schedule Offline Exam",
+  ],
+};
